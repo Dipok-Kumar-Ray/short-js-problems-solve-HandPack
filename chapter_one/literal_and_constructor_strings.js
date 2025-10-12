0e2c3467cd80f105cf3f733253652f0fc6854bb3
Dipok Kumar Ray <dipok7316@gmail.com>
2025-10-12 12:26:35 +0600

    Literal and Constructor Strings

---
+++ chapter_one/literal_and_constructor_strings.js
@@ -0,0 +1,12 @@
+// serial: 037
+
+var str = "Dipok Kumar Ray" //Literal String
+
+var n = 3;
+console.log(n.toString()); //Literal String
+
+var str2 = String("Constructor String"); //Constructor String
+
+console.log(str);
+
+console.log(str2);
