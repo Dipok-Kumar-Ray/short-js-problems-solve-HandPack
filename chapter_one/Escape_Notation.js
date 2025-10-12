8360ca1e1bbed66031836565af1e7ee02f4c78d3
Dipok Kumar Ray <dipok7316@gmail.com>
2025-10-12 12:34:45 +0600

    Escape  Notation

---
+++ chapter_one/Escape_Notation.js
@@ -0,0 +1,13 @@
+//serial: 038
+
+var str = "This is a 'String'";
+
+var str2 = "This is a \'String\'"
+
+var str2 = "This is a \\String";
+
+var str3 = "This is a \n String";
+
+var str3 = "This is a \t String";
+
+console.log(str, str2, str3);
